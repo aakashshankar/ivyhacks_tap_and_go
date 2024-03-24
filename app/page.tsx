cb95c3628ecb46c6b636a71c7d6ebb0190eb9530
Rohit Prakash <rohitp934@gmail.com>
2024-03-23 20:37:39 -0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Map from "@/components/map";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="text-red-500">Hello World!</div>
       <Button>Hello</Button>
-      <Map coordinates={[[-74.0060, 40.7128]]} locations={[]} routes={[]}/>
     </main>
   );
 }
