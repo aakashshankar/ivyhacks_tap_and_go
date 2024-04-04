f775a99406af33a51ed48a8f1734354ea447c7a3
Hansaem Park <49851536+sammitako@users.noreply.github.com>
2024-04-04 14:43:21 -0400

    Add spinner/loader

---
+++ app/itinerary/history/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Spinner from "../../../components/common/spinner";
+
+const page = () => {
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
+};
+
+export default page;
